1a2ab2c26fffc3b44bc5d5d40142e43865357aae
Haris-Sultan15 <163767894+Haris-Sultan15@users.noreply.github.com>
2024-05-28 17:17:24 +0400

    list card view

--- lib/main.dart
+++ lib/main.dart
@@ -1,76 +1,87 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Health-It',
-      debugShowCheckedModeBanner: false,
+      title: 'Random User Demo',
       theme: ThemeData(
-    
-        colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 58, 183, 141)),
-        useMaterial3: true,
+        primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Welcome To Health-It'),
+      home: RandomUserPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-
-  final String title;
-
+class RandomUserPage extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _RandomUserPageState createState() => _RandomUserPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 15;
+class _RandomUserPageState extends State<RandomUserPage> {
+  List<dynamic> users = [];
 
-  void _incrementCounter() {
-    setState(() {
-   
-      _counter--;
-    });
+  @override
+  void initState() {
+    super.initState();
+    fetchRandomUsers();
+  }
+
+  Future<void> fetchRandomUsers() async {
+    final response =
+        await http.get(Uri.parse('https://randomuser.me/api/?results=1200'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        users = json.decode(response.body)['results'];
+      });
+    } else {
+      throw Exception('Failed to load random users');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-       
-        // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-      
-        // title: Text(widget.title),
+        title: Text('Cardiology Doctors Lists'),
       ),
-      body: Center(
-    
-        child: Column(
-      
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            
-            
-
-// Image.asset('assests/img/healthit-logo.png'),
-Image.asset('assests/img/healthit-icon.png', width: 200, height: 200,),
-
-
-          ],
-        ),
-      ),
-      // This trailing comma makes auto-formatting nicer for build methods.
+      body: users.isEmpty
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (context, index) {
+                final user = users[index];
+                return Card(
+                  margin: const EdgeInsets.all(10),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          NetworkImage(user['picture']['thumbnail']),
+                    ),
+                    title: Text(
+                        '${user['name']['first']} ${user['name']['last']}'),
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(user['email']),
+                        Text('Phone: ${user['phone']}'),
+                        Text(
+                            'Location: ${user['location']['city']}, ${user['location']['country']}'),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
     );
   }
 }
