139d63adbc7961c52d6797ffb10d19ed85848a0e
Haris-Sultan15 <163767894+Haris-Sultan15@users.noreply.github.com>
2024-05-29 14:05:53 +0400

    button added

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,53 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'index.dart';  // Import the new page
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Random User Demo',
+      title: 'Country List',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: RandomUserPage(),
+      home: const MyHomePage(),
     );
   }
 }
 
-class RandomUserPage extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
   @override
-  _RandomUserPageState createState() => _RandomUserPageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _RandomUserPageState extends State<RandomUserPage> {
-  List<dynamic> users = [];
+class _MyHomePageState extends State<MyHomePage> {
+  List countries = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    fetchRandomUsers();
+    fetchCountries();
   }
 
-  Future<void> fetchRandomUsers() async {
-    final response =
-        await http.get(Uri.parse('https://randomuser.me/api/?results=1200'));
-
+  Future<void> fetchCountries() async {
+    final response = await http.get(Uri.parse('https://demolab2.hospogate.com/api/countries'));
     if (response.statusCode == 200) {
       setState(() {
-        users = json.decode(response.body)['results'];
+        countries = json.decode(response.body);
+        isLoading = false;
       });
     } else {
-      throw Exception('Failed to load random users');
+      throw Exception('Failed to load countries');
     }
   }
 
@@ -50,37 +55,56 @@ class _RandomUserPageState extends State<RandomUserPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Cardiology Doctors Lists'),
+        title: const Text('Country List'),
       ),
-      body: users.isEmpty
-          ? Center(
-              child: CircularProgressIndicator(),
-            )
-          : ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
-                return Card(
-                  margin: const EdgeInsets.all(10),
-                  child: ListTile(
-                    leading: CircleAvatar(
-                      backgroundImage:
-                          NetworkImage(user['picture']['thumbnail']),
-                    ),
-                    title: Text(
-                        '${user['name']['first']} ${user['name']['last']}'),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(user['email']),
-                        Text('Phone: ${user['phone']}'),
-                        Text(
-                            'Location: ${user['location']['city']}, ${user['location']['country']}'),
-                      ],
-                    ),
+      body: isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: countries.length,
+                    itemBuilder: (context, index) {
+                      final country = countries[index];
+                      return Card(
+                        margin: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          leading: SizedBox(
+                            width: 50,
+                            height: 50,
+                            child: Image.network(
+                              'https://via.placeholder.com/50', // Replace with actual flag URL if available
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          title: Text(country['name']),
+                          subtitle: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text('Country Code: ${country['code']}'),
+                              Text('Capital: ${country['capital']}'),
+                              Text('Region: ${country['region']}'),
+                              Text('Subregion: ${country['subregion']}'),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => const IndexPage()),
+                      );
+                    },
+                    child: const Text('Go to Index Page'),
+                  ),
+                ),
+              ],
             ),
     );
   }
