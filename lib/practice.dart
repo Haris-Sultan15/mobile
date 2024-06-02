59491f739b61c6553ef7e553020ba2923934bcaf
Haris-Sultan15 <163767894+Haris-Sultan15@users.noreply.github.com>
2024-06-02 12:20:43 +0400

    package added

--- lib/practice.dart
+++ lib/practice.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/doctor.dart';
+import 'package:country_picker/country_picker.dart';
 
 class PracticePage extends StatelessWidget {
   const PracticePage({Key? key}) : super(key: key);
