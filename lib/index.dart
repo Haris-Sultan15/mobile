056ac2b3dee98b08fccd344c4a7b1ecbde458aaf
Haris-Sultan15 <163767894+Haris-Sultan15@users.noreply.github.com>
2024-06-03 19:32:47 +0400

    bottom added

--- lib/index.dart
+++ lib/index.dart
@@ -5,11 +5,30 @@ import 'package:flutter_application_1/practice.dart';
 import 'package:country_picker/country_picker.dart';
 import 'counteries.dart';
 
-
-
-
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
   const IndexPage({Key? key}) : super(key: key);
+
+  @override
+  _IndexPageState createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  int _selectedIndex = 0;
+
+  // List of pages for navigation
+  final List<Widget> _pages = [
+    Center(child: Text('Home Page', style: TextStyle(color: const Color.fromARGB(255, 19, 14, 2)),)),
+    Center(child: Text('Doctors', style: TextStyle(color: const Color.fromARGB(255, 19, 14, 2)),)),
+     Center(child: Text('Practice', style: TextStyle(color: const Color.fromARGB(255, 19, 14, 2)),)),
+     Center(child: Text('Countries', style: TextStyle(color: const Color.fromARGB(255, 19, 14, 2)),)),
+      Center(child: Text('Settings', style: TextStyle(color: const Color.fromARGB(255, 19, 14, 2)),)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -40,64 +59,74 @@ class IndexPage extends StatelessWidget {
               },
               child: const Text('Doctors Lists'),
             ),
-            
-            // ElevatedButton(onPressed: () 
-            // {
-            //   Navigator.pop(
-            //     context,
-            //     MaterialPageRoute(builder: (context) => const TestPage()));
-            // },
-            //  child: Text('Go To Practice Page')),
-               InkWell(
-            onDoubleTap: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => const DoctorPage()));
-            },
-             child: Text('i am inkwell widget please double tap on me')
-          ),
-              //  ElevatedButton
-              //   (onPressed: () {
-              //     Navigator.pop
-              //     (context, 
-              //     MaterialPageRoute(builder: (context) => const TestPage()),
-                  
-              //     );
-              //   }, 
-              //   child: Text('Test Page'),),
-             ElevatedButton
-                (onPressed: () {
-                  Navigator.pop
-                  (context, 
+            InkWell(
+              onDoubleTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const DoctorPage()));
+              },
+              child: Text('I am inkwell widget please double tap on me'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
                   MaterialPageRoute(builder: (context) => const CountriesPage()),
-                  
-                  );
-                }, 
-                child: Text('Select Country'),),
-
-                 InkWell(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
-                  },
-                  child: Text('Double Tap on me'),
-                ),
-SizedBox(height: 16),
-          ElevatedButton(onPressed: () {
-Navigator.push(
-  context, MaterialPageRoute(builder: (context) => const PracticePage()),
-);
-
-          }, child: Text('Go to practice page')),
-            ElevatedButton(onPressed: () {
-Navigator.push(
-  context, MaterialPageRoute(builder: (context) => const CountriesPage()),
-);
-
-          }, child: Text('Select Your Country')),
-        
-          
+                );
+              },
+              child: Text('Select Country'),
+            ),
+            InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => MyHomePage()));
+              },
+              child: Text('Double Tap on me'),
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => const PracticePage()),
+                );
+              }, 
+              child: Text('Go to practice page')
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => const CountriesPage()),
+                );
+              }, 
+              child: Text('Select Your Country')
+            ),
           ],
         ),
       ),
-      
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.medical_services),
+            label: 'Doctors',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'Practice',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.public),
+            label: 'Countries',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Color.fromARGB(255, 24, 26, 168),
+        onTap: _onItemTapped,
+      ),
     );
   }
 }
